b088502ed678444ad582e22f6c6f83928acbfc25
filipprvulj <filip.prvulj@gmail.com>
2020-01-22 22:38:45 +0100

    Izmena na index view PreduzecesController

--- RegistarPreduzecaV11.0/Models/Preduzece.cs
+++ RegistarPreduzecaV11.0/Models/Preduzece.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace RegistarPreduzecaV11._0.Models
 {
@@ -9,12 +10,16 @@ namespace RegistarPreduzecaV11._0.Models
 	{
 		public int Id { get; set; }
 
+		[Display(Name = "Registrovani naziv")]
 		public string RegNaziv { get; set; }
 
+		[Display(Name = "Registrovana adresa")]
 		public string RegAdresa { get; set; }
 
+		[Display(Name = "Opština")]
 		public string Opstina { get; set; }
 
+		[Display(Name = "Poštaniski broj")]
 		public int PostanskiBroj { get; set; }
 
 		public string MaticniBroj { get; set; }
@@ -23,16 +28,20 @@ namespace RegistarPreduzecaV11._0.Models
 
 		public int SifraDelatnosti { get; set; }
 
+		[Display(Name = "Opis delatnosti")]
 		public string OpisDelatnosti { get; set; }
 
+		[Display(Name = "Broj računa")]
 		public string BrojRacuna { get; set; }
 
+		[Display(Name = "Internet stranica")]
 		public string WebStranica { get; set; }
 
 		public ICollection<KontaktOsoba> KontaktOsobe { get; set; }
 
 		public string Pecat { get; set; }
 
+		[Display(Name = "Beleška")]
 		public string Beleska { get; set; }
 	}
 }
