2bb5a27e270bd0db1feb67a3792129bdf9f997ee
filipprvulj <filip.prvulj@gmail.com>
2020-01-27 21:30:06 +0100

    KontaktOsobaValidacija

---
+++ RegistarPreduzecaV11.0/Migrations/202001272019045_ValidacijaKontaktOsoba.cs
@@ -0,0 +1,22 @@
+﻿namespace RegistarPreduzecaV11._0.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ValidacijaKontaktOsoba : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.KontaktOsobas", "Ime", c => c.String(nullable: false, maxLength: 30));
+            AlterColumn("dbo.KontaktOsobas", "Prezime", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.KontaktOsobas", "RadnoMesto", c => c.String(nullable: false, maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.KontaktOsobas", "RadnoMesto", c => c.String());
+            AlterColumn("dbo.KontaktOsobas", "Prezime", c => c.String());
+            AlterColumn("dbo.KontaktOsobas", "Ime", c => c.String());
+        }
+    }
+}
