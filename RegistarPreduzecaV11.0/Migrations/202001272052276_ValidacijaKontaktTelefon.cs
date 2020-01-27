c3bda9f5f43b3e824e826a5ae5f675e6fad86f0d
filipprvulj <filip.prvulj@gmail.com>
2020-01-27 22:14:50 +0100

    KontaktTelefon validacija

---
+++ RegistarPreduzecaV11.0/Migrations/202001272052276_ValidacijaKontaktTelefon.cs
@@ -0,0 +1,22 @@
+﻿namespace RegistarPreduzecaV11._0.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ValidacijaKontaktTelefon : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.KontaktTelefons", "OznakaTipa", c => c.String(nullable: false, maxLength: 25));
+            AlterColumn("dbo.KontaktTelefons", "BrojTelefona", c => c.String(nullable: false));
+            AlterColumn("dbo.KontaktTelefons", "Lokal", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.KontaktTelefons", "Lokal", c => c.Int(nullable: false));
+            AlterColumn("dbo.KontaktTelefons", "BrojTelefona", c => c.String());
+            AlterColumn("dbo.KontaktTelefons", "OznakaTipa", c => c.String());
+        }
+    }
+}
